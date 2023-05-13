c927a0e528ddc784be825d0044c01c94c94c237b
mikailaliyev <mikail.aliyev@gmail.com>
2023-05-13 13:45:25 +0400

    added: serving index.html at main route

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import random
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-async def root():
-    return {"example": "This is an example", "data": 999}
+def form_post(request: Request):
+    result = "Type a number"
+    return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
+
 
 @app.get("/random/{rand_number}")
 async def get_random(rand_number):
