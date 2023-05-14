4cd4d709dafd010342e30a3edaaed5352c0f3448
mikailaliyev <mikail.aliyev@gmail.com>
2023-05-14 15:51:04 +0400

    added: static files serving option

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import random
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def form_post(request: Request):
@@ -14,4 +16,4 @@ def form_post(request: Request):
 
 @app.get("/{rand_number}")
 async def get_random(rand_number):
-    return random.randint(9, int(rand_number))
+    return random.randint(0, int(rand_number))
