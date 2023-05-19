7f8f2f84cc2ede28c2fda196e0d1327f07db43e4
Mikayil <mikail.aliyev@gmail.com>
2023-05-19 14:40:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import random
 
+if __name__ == "__main__":
+    uvicorn.run("test_fastapi:app", host="0.0.0.0", port=5000, log_level="info")
+
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
