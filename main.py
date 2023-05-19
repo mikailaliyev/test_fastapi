53e3fbcec5843a9bf47d63e860ab7f4cf25062c2
Mikayil <mikail.aliyev@gmail.com>
2023-05-19 15:26:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-import random
-import uvicorn
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="{{ url_for('static', path='/images/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ url_for('static', path='/styles.css') }}" />
+    <title>Sample Page</title>
+  </head>
+  <body>
+    <h1>Random Number Generator</h1>
+    <p>
+      Hello, this is just a test backend app for getting random number in the range of 0 to the number of your choice
+      using FastAPI.
+    </p>
+    <p>This is the example usage of the app in your application in JavaScript:</p>
+    <pre>
+const random_number = 99
+url = `https://test-fastapi-app.vercel.app/${random_number}`
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+const handleData = async () => {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/")
-def form_post(request: Request):
-    result = "Type a number"
-    return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
-
-
-@app.get("/{rand_number}")
-async def get_random(rand_number):
-    return random.randint(0, int(rand_number))
+handleData()
+    </pre>
+    <p>Or you can test it right in your browser:</p>
+    <p>https://test-fastapi-app.vercel.app/random number</p>
+  </body>
+</html>
