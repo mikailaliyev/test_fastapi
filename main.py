4ca5f13463970d94844e5d91c96812a53bb8ef01
Mikayil <mikail.aliyev@gmail.com>
2023-05-19 15:52:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import random
@@ -17,8 +17,9 @@ def form_post(request: Request):
     result = "Type a number"
     return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
 
+
 @app.get("/{rand_number}")
-async def get_random(rand_number):
-    if rand_number.isnumeric() == False:
-        raise HTTPException(status_code=404, detail="You should enter only integers")
-    return random.randint(0, int(rand_number))
+async def get_random(rand_number):  
+  if rand_number.isnumeric() == False:
+    raise HTTPException(status_code=404, detail="You should enter only integers")
+  return random.randint(0, int(rand_number))
